087fd5890844612eff93b35cad0721faa71cf9dd
Orgher Dalipi <orgher.dalipi@gmail.com>
2023-01-03 17:47:47 +0100

    Update resources

--- 2.object-oriented-programming/4.exceptions/ArgumentValidation.java
+++ 2.object-oriented-programming/4.exceptions/ArgumentValidation.java
@@ -1,19 +1,51 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ArgumentValidation {
 
     public static void main(String[] args) {
 
+        // config scanner
         Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.ENGLISH);
 
         User user = new User();
         System.out.println("We are setting up your user account.");
 
         System.out.print("Your username is currently " + user.getUsername() + ". Please update it here: ");
-        user.setUsername(user.getUsername());
-        
-        System.out.print("Your age is currently " + user.getAge() + ". Please update it here: ");
-        user.setAge(scanner.nextInt());
+        while (true) {
+            try {
+                user.setUsername(scanner.nextLine());
+                break;
+            } catch (IllegalArgumentException ex) {
+                System.out.print(ex.getMessage() + ". Pleas enter your username again: ");
+            }
+        }
+
+        System.out.print("\nYour age is currently " + user.getAge() + ". Please update it here: ");
+        while (true) {
+            try {
+                user.setAge(scanner.nextInt());
+                break;
+            } catch (Exception ex) {
+                String exMessage = ex.getMessage();
+                if (exMessage == null) {
+                    exMessage = "";
+                } else {
+                    exMessage +=  ". ";
+                }
+                System.out.print(exMessage + "Pleas enter your age again again: ");
+                scanner.nextLine();
+            }
+        }
+
+        // print account confirmation data
+        System.out.println(
+                "\nYour account was created successfully:" + "\n" +
+                        "\t" + "Username: " + user.getUsername() + "\n" +
+                        "\t" + "Age: " + user.getAge());
+
+        // close scanner
         scanner.close();
-    }     
+    }
 }
--- 2.object-oriented-programming/4.exceptions/User.java
+++ 2.object-oriented-programming/4.exceptions/User.java
@@ -7,7 +7,10 @@ public class User {
         return this.age;
     }
 
-    public void setAge(int age) { 
+    public void setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("User can not have negative age");
+        }
         this.age = age;
     }
 
@@ -16,8 +19,10 @@ public class User {
     }
 
     public void setUsername(String username) {
+        if (username == null || username.isBlank()) {
+            throw new IllegalArgumentException("User name value is not valid");
+        }
         this.username = username;
     }
-
 
 }
