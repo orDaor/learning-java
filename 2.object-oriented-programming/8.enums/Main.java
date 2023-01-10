ea67d5ed8236040fb2ee5063623506e5e0c6c17b
Orgher Dalipi <orgher.dalipi@gmail.com>
2023-01-10 15:38:58 +0100

    Enums

--- 2.object-oriented-programming/8.enums/Main.java
+++ 2.object-oriented-programming/8.enums/Main.java
@@ -1,10 +1,17 @@
 import model.Car;
+import model.Car.TrafficLight;
 
 public class Main {
 
     public static void main(String[] args) {
+        //new car
         Car car = new Car("Nissan", 2020);
-        car.drive("RED");
+
+        //pass as a parameter one of the values of the TrafficLight enum, which is
+        //a static member of Car class
+        // car.drive(Car.TrafficLight.RED);
+        TrafficLight light = null; //Car.TrafficLight.YELLOW;
+        car.drive(light);
     }
 
 }
--- 2.object-oriented-programming/8.enums/model/Car.java
+++ 2.object-oriented-programming/8.enums/model/Car.java
@@ -1,13 +1,23 @@
 package model;
+
 public class Car {
+    // properties
     private String make;
     private int year;
 
+    // enum property, it is STATIC by DEFAULT
+    //NOTE: enum values are still STATIC & final (constants) members of the enum
+    public enum TrafficLight {
+        GREEN, YELLOW, RED
+    };
+
+    // constructor
     public Car(String make, int year) {
         this.make = make;
         this.year = year;
     }
 
+    // getters and setters
     public String getMake() {
         return this.make;
     }
@@ -24,26 +34,32 @@ public class Car {
         this.year = year;
     }
 
-    public void drive(String trafficLight) {
-        if (!trafficLight.equals("RED") && !trafficLight.equals("GREEN") && !trafficLight.equals("YELLOW")) {
-            throw new IllegalArgumentException("TRAFFIC LIGHT CAN ONLY BE RED, GREEN, YELLOW");
-        }
+    //this will as a parameter one of the 3 values of the TrafficLight enum
+    public void drive(TrafficLight trafficLight) {
 
         switch (trafficLight) {
-            case "RED": System.out.println("STOP"); break;
-            case "GREEN": System.out.println("DRIVE"); break;
-            case "YELLOW": System.out.println("SLOW DOWN"); break;
+            case RED:
+                System.out.println("STOP");
+                break;
+            case GREEN:
+                System.out.println("DRIVE");
+                break;
+            case YELLOW:
+                System.out.println("SLOW DOWN");
+                break;
+            default:
+                throw new IllegalArgumentException("Bad traffic light value");
         }
     }
 
     // public void drive(TrafficLight trafficLight) {
-    //     if (trafficLight == null) throw new IllegalArgumentException("Traffic Light cannot be null");
-    //     switch (trafficLight) {
-    //         case RED: System.out.println("STOP"); break;
-    //         case GREEN: System.out.println("DRIVE"); break;
-    //         case YELLOW: System.out.println("SLOW DOWN"); break;
-    //     }
+    // if (trafficLight == null) throw new IllegalArgumentException("Traffic Light
+    // cannot be null");
+    // switch (trafficLight) {
+    // case RED: System.out.println("STOP"); break;
+    // case GREEN: System.out.println("DRIVE"); break;
+    // case YELLOW: System.out.println("SLOW DOWN"); break;
     // }
-
+    // }
 
 }
